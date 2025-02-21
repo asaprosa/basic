25b297b357ee8c7b4aad382410c3d6d9ce3d3c20
Aditya Arun ghodke <91055197+asaprosa@users.noreply.github.com>
2025-02-22 00:12:58 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Sizes of Data Types:\n");
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    
+    return 0;
+}
