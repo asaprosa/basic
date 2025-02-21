4fb2113b7b9d820f63f958c157be1462e8690eb6
Aditya Arun ghodke <91055197+asaprosa@users.noreply.github.com>
2025-02-22 00:12:03 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 20, num2 = 10;
+    
+    printf("Arithmetic Operations:\n");
+    printf("Addition: %d + %d = %d\n", num1, num2, num1 + num2);
+    printf("Subtraction: %d - %d = %d\n", num1, num2, num1 - num2);
+    printf("Multiplication: %d * %d = %d\n", num1, num2, num1 * num2);
+    printf("Division: %d / %d = %d\n", num1, num2, num1 / num2);
+    printf("Modulus: %d %% %d = %d\n", num1, num2, num1 % num2);
+    
+    return 0;
+}
